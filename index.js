dfd599ead57e779739b518c928a13363dc03b8c5
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-12 18:24:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+
+
+
+const textToChange = document.querySelector("#text")
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    textToChange.innerHTML = "This is really cool!";
+});
+
+

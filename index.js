e5bc56c6fcb4a4fb5db5b828cff02ba390728dc3
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 15:09:43 -0500

    commit tree 85b923ecb72811346995439f427aa7b144247ec8

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// Your code goes here
+// // Your code goes here
 
 
 
 
 
-// const textToChange = document.querySelector("#text")
+// // const textToChange = document.querySelector("#text")
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     textToChange.innerHTML = "This is really cool!";
-// });
-
-
-
-
-
-
+// // document.addEventListener("DOMContentLoaded", function() {
+// //     textToChange.innerHTML = "This is really cool!";
+// // });
 
 
 
@@ -29,17 +23,35 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function(e) {
-    const newPar = document.querySelector('#text')
-    newPar.textContent = "This is really cool!" 
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function(e) {
+//     const newPar = document.querySelector('#text')
+//     newPar.textContent = "This is really cool!" 
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+modP = document.querySelector('#text')
+
+modP.addEventListener('DOMContentLoaded', function(e) {
+    e.target.value = "This is really cool"
+    console.log(e.target)
 })
-
-
-
-
-
-
-
-
-
-

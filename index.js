1c6e5318e975fc8a090bd6c5643ad4996195a0a7
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 15:44:17 -0500

    done

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// Your code goes here
+// // Your code goes here
 
 
 
 
 
-// const textToChange = document.querySelector("#text")
+// // const textToChange = document.querySelector("#text")
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     textToChange.innerHTML = "This is really cool!";
-// });
-
-
-
-
-
-
+// // document.addEventListener("DOMContentLoaded", function() {
+// //     textToChange.innerHTML = "This is really cool!";
+// // });
 
 
 
@@ -28,18 +22,37 @@
 
 
 
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function(e) {
+//     const newPar = document.querySelector('#text')
+//     newPar.textContent = "This is really cool!" 
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+modP = document.querySelector('#text')
 
 document.addEventListener('DOMContentLoaded', function(e) {
-    const newPar = document.querySelector('#text')
-    newPar.textContent = "This is really cool!" 
+    //debugger
+    modP.innerText = "This is really cool"
+    //console.log(e.target)
 })
-
-
-
-
-
-
-
-
-
-
